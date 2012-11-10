ba9bd0a1b03314b3ac586e51c6a820d96a63dea9
mian.muhammad.mubasher@gmail.com <mian.muhammad.mubasher@gmail.com@a5cea1d4-f70c-d3a3-ed90-bcad81743496>
2012-11-10 06:58:39 +0000

    Servlet Chaining Demo

---
+++ trunk/ServletChainingDemo/src/pk/edu/pucit/chainingdemo/ChainedServlet.java
@@ -0,0 +1,44 @@
+package pk.edu.pucit.chainingdemo;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ChainedServlet
+ */
+@WebServlet("/ChainedServlet")
+public class ChainedServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ChainedServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		PrintWriter out = response.getWriter();
+		out.println("<h1>Welcome to Chained Servlet</h1>");
+		out.println("<h1>Good Bye from Chained Servlet</h1>");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
