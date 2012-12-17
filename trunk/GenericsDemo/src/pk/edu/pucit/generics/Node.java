7c296dc086d2d0b34e08ec7748d591fdc9499c9a
mian.muhammad.mubasher@gmail.com <mian.muhammad.mubasher@gmail.com@a5cea1d4-f70c-d3a3-ed90-bcad81743496>
2012-12-17 12:35:30 +0000

    am just bore of this demo thing :@

---
+++ trunk/GenericsDemo/src/pk/edu/pucit/generics/Node.java
@@ -0,0 +1,19 @@
+package pk.edu.pucit.generics;
+
+
+public class Node<T> {
+	private T element;
+	private Node<T> next;
+	public T getElement() {
+		return element;
+	}
+	public void setElement(T element) {
+		this.element = element;
+	}
+	public Node<T> getNext() {
+		return next;
+	}
+	public void setNext(Node<T> next) {
+		this.next = next;
+	}
+}
